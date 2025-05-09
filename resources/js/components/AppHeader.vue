8fba3cc3dea4b758f11e5b46261a97687a841f09
Aleksandr Smirnov <smirnov_aleksandr@yahoo.com>
2025-05-09 20:11:12 +0300

    renamed 'dashboard' to 'chat' route

--- resources/js/components/AppHeader.vue
+++ resources/js/components/AppHeader.vue
@@ -40,8 +40,8 @@ const activeItemStyles = computed(
 
 const mainNavItems: NavItem[] = [
     {
-        title: 'Dashboard',
-        href: '/dashboard',
+        title: 'Chat',
+        href: '/chat',
         icon: LayoutGrid,
     },
 ];
@@ -108,7 +108,7 @@ const rightNavItems: NavItem[] = [
                     </Sheet>
                 </div>
 
-                <Link :href="route('dashboard')" class="flex items-center gap-x-2">
+                <Link :href="route('chat')" class="flex items-center gap-x-2">
                     <AppLogo />
                 </Link>
 
