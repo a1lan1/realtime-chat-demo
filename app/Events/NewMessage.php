b2ed78d68a693a2baf338cce96985522b8d4a4d4
Aleksandr Smirnov <smirnov_aleksandr@yahoo.com>
2025-05-09 20:36:37 +0300

    added NewMessage event with room broadcast channel

---
+++ app/Events/NewMessage.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Message;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class NewMessage implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(public Message $message) {}
+
+    public function broadcastOn(): Channel
+    {
+        return new Channel('room.'.$this->message->room_id);
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'message' => $this->message
+                ->loadMissing('user')
+                ->toArray(),
+        ];
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'message.new';
+    }
+}
--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('room.{roomId}', function (User $user, int $roomId) {
+    return auth()->check();
 });
