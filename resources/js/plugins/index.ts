bf5fb8655c782f6c287484e972c83c5263c4b9c9
Aleksandr Smirnov <smirnov_aleksandr@yahoo.com>
2025-05-10 09:05:42 +0300

    added pinia plugin with persisted state

--- resources/js/plugins/index.ts
+++ resources/js/plugins/index.ts
@@ -1,9 +1,11 @@
 import echo from './echo'
+import pinia from './pinia'
 import { ZiggyVue } from 'ziggy-js'
 import type { App } from 'vue'
 
 export function registerPlugins(app: App) {
   app
     .use(echo)
+    .use(pinia)
     .use(ZiggyVue)
 }
